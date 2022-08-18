394fddcaf6a1172061ebec9fcef28e909c76c59f
SpreeHertz <spreehertzofficial@gmail.com>
2022-08-18 23:01:15 +0530

    feat: cleaner code and better console logging

--- README.md
+++ README.md
@@ -18,3 +18,5 @@ Go to `config.json`:
 FIll in the above fields. You can obtain a spotify token from the [Spotify Developer Console](https://developer.spotify.com/console/get-users-currently-playing-track).
 
 Leave `current_bio` empty if you only want to show the currently playing song.
+
+> Note: Use it at your own risk. Don't abuse the APIs. Your account may get blocked by Instagram.
--- index.js
+++ index.js
@@ -1,51 +1,50 @@
 const fetch = require('node-fetch');
+const config = require('./config.json')
+const chalk = require('chalk');
 const { IgApiClient } = require("instagram-private-api")
 const ig = new IgApiClient()
 require('dotenv').config();
-const config = require('./config.json')
 
 const USERNAME = process.env.instagram_username || config.instagram_username;
 const PASSWORD = process.env.instagram_password || config.instagram_password;
+const currentBio = process.env.current_bio || config.current_bio;
+const sentenceBeforeMentioningTrack = process.env.sentence_before_mentioning_track || config.sentence_before_mentioning_track;
 
-ig.state.generateDevice(USERNAME)
 
-const main = async () => {
-    fetch("https://api.spotify.com/v1/me/player/currently-playing?market=ES", {
+ig.state.generateDevice(USERNAME);
+
+const response = fetch("https://api.spotify.com/v1/me/player/currently-playing?market=ES", {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
             'Authorization': `Bearer ${process.env.spotify_token || config.spotify_token}`,
         },
-    }).then(async response => {
-        // Try to get a sucessful response
-        try {
-            const res = await response.json();
-            const currentlyPlayingTrack = res.item.name;
-            const currentlyPlayingArtist = res.item.artists[0].name;
-        console.log(`Currently playing: ${currentlyPlayingTrack} by ${currentlyPlayingArtist}`);
-        // Try to change the bio of the user
-        try {
-            await ig.simulate.preLoginFlow()
-            await ig.account.login(USERNAME, PASSWORD)
-            // log out of Instagram when done
-            process.nextTick(async () => await ig.simulate.postLoginFlow())
-    
-            await ig.account.setBiography(`${config.current_bio}\n▶ Playing: ${currentlyPlayingTrack} - ${currentlyPlayingArtist}`)
-        }
-        catch (err) {
-            console.log(err);
-        }
-        // If there was an error fetching the track itself, change the bio to the default one
-        }
-        catch (err) {
-            await ig.simulate.preLoginFlow()
-            await ig.account.login(USERNAME, PASSWORD)
-            process.nextTick(async () => await ig.simulate.postLoginFlow())
-            await ig.account.setBiography(`${config.current_bio}`)
-        }
     })
+
+const login = async () => {
+    try {
+        await ig.simulate.preLoginFlow()
+        await ig.account.login(USERNAME, PASSWORD)
+        process.nextTick(async () => await ig.simulate.postLoginFlow())
+    }
+    catch (err) {
+        console.log(chalk.redBright('error' + " An error occured while connecting to your Instagram account:" + err))
+    }
+}
+
+const main = async () => {
+    try {
+    const data = await response.json();
+    const track = data.item.name;
+    const artist = data.item.artists[0].name;
+    console.log(chalk.blueBright('playing') + chalk.yellow(` ${track}`) + ` -` + chalk.yellowBright(` ${artist}`));
+    login().then(await ig.account.setBiography(`${currentBio}` + `${sentenceBeforeMentioningTrack}` + ` ${track} - ${artist}`))    
+    }
+    catch (err) {
+        login().then(await ig.account.setBiography(`${currentBio}`))
+    }
+    
 }
 
 main();
-
